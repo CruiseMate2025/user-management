de0d781cf48efbfbbd630dd7c5951a8f0a572f22
Ali <2464292@cognizant.com>
2026-02-26 15:46:19 +0530

    untracted files push which is comming from the pull

--- src/main/java/org/genc/usermgmt/filter/JwtAuthenticationFilter.java
+++ src/main/java/org/genc/usermgmt/filter/JwtAuthenticationFilter.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.genc.usermgmt.dto.CustomUserDetails;
 import org.genc.usermgmt.service.impl.CustomUserDetailsService;
 import org.genc.usermgmt.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -25,8 +26,9 @@ import java.util.List;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    @Autowired
     private final JwtUtil jwtUtil;
-
+    @Autowired
     private final CustomUserDetailsService userDetailsService;
 
 
